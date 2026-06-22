9d61f7fa7dc83609d8471de8038a9305a03daffb
Aaditya Singh <aaditya3668singh@gmail.com>
2026-06-22 23:49:30 +0530

    Refactor Navbar component to include Settings link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,16 +22,16 @@ const Navbar = () => {
           </div>
 
           <div className="flex items-center gap-2">
-            <Link
+            {/* <Link
               to={"/settings"}
               className={`
               btn btn-sm gap-2 transition-colors
               
               `}
             >
-              {/* <Settings className="w-4 h-4" />
-              <span className="hidden sm:inline">Settings</span> */}
-            </Link>
+              <Settings className="w-4 h-4" />
+              <span className="hidden sm:inline">Settings</span> 
+            </Link> */}
 
             {authUser && (
               <>
